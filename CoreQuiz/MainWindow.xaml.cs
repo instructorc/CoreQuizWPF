c4c8334a7c861c1be427a4e1a8ac7bce9c10c9ce
fulchr <cfulton@dunwoody.edu>
2019-03-06 11:41:43 -0600

    changing stuff

--- CoreQuiz/MainWindow.xaml.cs
+++ CoreQuiz/MainWindow.xaml.cs
@@ -20,9 +20,33 @@ namespace CoreQuiz
     /// </summary>
     public partial class MainWindow : Window
     {
+        int count = 0;
+        string[] questions = { "When was .NET core released", "What is the current version of .NET core" };
+        string[,] qOptions = {
+                                    {"A) 2019", "B) 2014", "C) 2003", "D)1990"},
+                                    {"A) 1.0", "B) 2.2", "C) 10.0", "D)3.0" }
+                              };
+        string[] storedAnswers = { "B) 2014", "B) 2.2" };
+
+        string[] holdUserAnwsers = new string[2];
+
+        List<string> wrong = new List<string>();
+        List<string> correct = new List<string>();
+
         public MainWindow()
         {
             InitializeComponent();
+            Prev_btn.Visibility = Visibility.Hidden;
+            question_txt.Text = questions[count].ToString();
+            var radioButtonList = panel_one.Children.OfType<RadioButton>();
+
+            //Iterate through the child radio buttons and assign them values within the array
+            for(var x = 0; x < radioButtonList.Count<RadioButton>(); x++)
+            {
+                radioButtonList.ElementAt<RadioButton>(x).Content = qOptions[count, x];
+
+            }
+
         }
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
@@ -33,14 +57,94 @@ namespace CoreQuiz
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
+            for(var w = 0; w < holdUserAnwsers.Length; w++)
+            {
+                if(holdUserAnwsers[w] == storedAnswers[w])
+                {
+                    correct.Add(holdUserAnwsers[w]);
+                }
+            }
+
+            if(correct.Count > 1)
+            {
+                MessageBox.Show("You passed!!");
+            }
+            else
+            {
+                MessageBox.Show("You Failed");
+            }
+
         }
 
         private void NextButton_Click(object sender, RoutedEventArgs e)
         {
-            Results aWindow = new Results();
-            aWindow.Show();
-            this.Close();
 
+
+            
+            Prev_btn.Visibility = Visibility.Visible;
+            count++;
+            if (quizStatus.Value < quizStatus.Maximum)
+
+            {
+
+                quizStatus.Value++;
+
+            }
+            var radioButtonList = panel_one.Children.OfType<RadioButton>();
+            foreach(var p in radioButtonList)
+            {
+                if(p.IsChecked == true)
+                {
+                    holdUserAnwsers[count-1] = p.Content.ToString();
+                }
+            }
+            MessageBox.Show(holdUserAnwsers[count - 1]);
+            if (count < questions.Length)
+            {
+                question_txt.Text = questions[count].ToString();
+               
+
+                //Iterate through the child radio buttons and assign them values within the array
+                for (var x = 0; x < radioButtonList.Count<RadioButton>(); x++)
+                {
+                    radioButtonList.ElementAt<RadioButton>(x).Content = qOptions[count, x];
+
+                }
+            }
+            //Results aWindow = new Results();
+            //aWindow.Show();
+            //this.Close();
+
+        }
+
+        private void PrevButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            count--;
+            if (quizStatus.Value > quizStatus.Minimum)
+
+            {
+
+                quizStatus.Value--;
+
+            }
+            if (count == 0)
+            {
+                Prev_btn.Visibility = Visibility.Hidden;
+            }
+           
+            if (count < questions.Length )
+            {
+                question_txt.Text = questions[count].ToString();
+                var radioButtonList = panel_one.Children.OfType<RadioButton>();
+
+                //Iterate through the child radio buttons and assign them values within the array
+                for (var x = 0; x < radioButtonList.Count<RadioButton>(); x++)
+                {
+                    radioButtonList.ElementAt<RadioButton>(x).Content = qOptions[count, x];
+
+                }
+            }
         }
     }
 }
